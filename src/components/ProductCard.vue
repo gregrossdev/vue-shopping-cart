c0a2ec1a3349eb2695c3fcab0a2f5615f0371cfb
Gregory Ross <gregross.dev@gmail.com>
2023-06-18 15:21:23 -0400

    init

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useCartStore } from '@/stores/cart.js';
+import { toCurrency } from '@/shared/utils.js'
+import { defineProps } from 'vue';
+
+const cartStore = useCartStore()
+defineProps({
+  product: Object
+})
+</script>
+
+
+<template>
+    <div class="card bordered ">
+        <figure class="px-8 pt-10">
+            <img
+                :src="product.image"
+                alt="Card Image"
+                class="object-contain w-full h-64"
+            />
+        </figure> 
+        <div class="card-body">
+            <h2 class="card-title">
+                <router-link class="link link-hover" :to="`/product/${product.id}`">{{ product.title }}</router-link>
+            </h2>
+            <p>{{ toCurrency(product.price) }}</p>
+            <div class="justify-end card-actions">
+                <button class="btn btn-primary" @click="cartStore.add(product.id)">Add to Cart</button>
+            </div>
+        </div>
+    </div>   
+</template>
+
---
+++ src/stores/products.js
@@ -0,0 +1,35 @@
+import { defineStore } from 'pinia';
+
+const fakeStoreUrl = 'https://fakestoreapi.com';
+
+export const useProductStore = defineStore({
+  id: 'products',
+
+  state: () => ({
+    items: {},
+    ids: [],
+  }),
+
+  getters: {
+    list() {
+      return this.ids.map((i) => this.items[i]);
+    },
+
+    loaded() {
+      return this.ids.length > 0;
+    },
+  },
+
+  actions: {
+    async fetchAll() {
+      if (this.loaded) return;
+
+      const res = await fetch(`${fakeStoreUrl}/products`);
+      const data = await res.json();
+      this.ids = data.map((product) => {
+        this.items[product.id] = product;
+        return product.id;
+      });
+    },
+  },
+});
